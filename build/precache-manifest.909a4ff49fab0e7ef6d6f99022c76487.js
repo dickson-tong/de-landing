5832a882af40985ed073b0a65be9b6483e4928d9
Dickson Tong <furiousbubbles@Dicksons-MacBook-Pro.local>
2020-06-19 22:17:11 -0700

    update build

---
+++ build/precache-manifest.909a4ff49fab0e7ef6d6f99022c76487.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a638c84484c0386b940d66092d71b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "975eb98d60f744a5e75e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "046120221d31283c4dd5",
+    "url": "/static/js/2.e061f11a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e061f11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975eb98d60f744a5e75e",
+    "url": "/static/js/main.a5d39d09.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "af2a95daff37dccc340003f07d9f51c4",
+    "url": "/static/media/business-owner.af2a95da.jpg"
+  },
+  {
+    "revision": "dea1882919b18fd4bcfddb26e4986f32",
+    "url": "/static/media/header.dea18829.jpg"
+  },
+  {
+    "revision": "ca7056ca45026686068125fa7ca9e70e",
+    "url": "/static/media/mounted.ca7056ca.jpg"
+  },
+  {
+    "revision": "d4882a073bd1fcfe68da3f281b257146",
+    "url": "/static/media/relax.d4882a07.jpg"
+  },
+  {
+    "revision": "0b06091d14787445bda58e574d201ae7",
+    "url": "/static/media/standing.0b06091d.jpg"
+  }
+]);

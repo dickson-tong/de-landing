51cfd1a41ce933a5bfc6c5b564783b727e132ce3
Dickson Tong <furiousbubbles@Dicksons-MacBook-Pro.local>
2020-06-20 21:45:10 -0700

    first pass at responsive layout

--- src/App.js
+++ src/App.js
@@ -1,81 +1,150 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 
-import './App.css';
-import HeaderImage from './header.jpg';
-import BusinessOwnerImage from './business-owner.jpg';
-import RelaxImage from './relax.jpg';
-import MountedImage from './mounted.jpg';
-import StandingImage from './standing.jpg';
-import Logo from './vector_logo.png';
+import "./App.css";
+import HeaderImage from "./header.jpg";
+import BusinessOwnerImage from "./business-owner.jpg";
+import RelaxImage from "./relax.jpg";
+import MountedImage from "./mounted.jpg";
+import StandingImage from "./standing.jpg";
+import Logo from "./vector_logo.png";
 
-const EDMUND_GRAY = '#e1e3ea';
-const MIDDLE_BLUE_GREEN = '#ffc6b7';
-const GREEN_SHEEN = '#8BB8A8';
-const PINK_LACE = '#FFD5FF';
+const EDMUND_GRAY = "#e1e3ea";
+const MIDDLE_BLUE_GREEN = "#ffc6b7";
+const PAGE_PADDING = "30px";
 
 const TH = (props) => {
   return (
-    <th style={{backgroundColor: EDMUND_GRAY, border: `1px ${EDMUND_GRAY} solid`, padding: '6px', fontSize: '20px'}}>{props.children}</th>
+    <th
+      style={{
+        backgroundColor: EDMUND_GRAY,
+        border: `1px ${EDMUND_GRAY} solid`,
+        padding: "6px",
+        fontSize: "20px",
+      }}
+    >
+      {props.children}
+    </th>
   );
 };
 
 const TD = (props) => {
   return (
-    <td {...props} style={{border: `1px ${EDMUND_GRAY} solid`,  padding: '6px', fontSize: '20px', ...props.$style}}>{props.children}</td>
+    <td
+      {...props}
+      style={{
+        border: `1px ${EDMUND_GRAY} solid`,
+        padding: "6px",
+        fontSize: "20px",
+        ...props.$style,
+      }}
+    >
+      {props.children}
+    </td>
   );
 };
 
-
 const MenuItem = (props) => {
   //replace with link
-  const {location} = props;
+  const { location } = props;
   return (
-    <Link to={location} style={{marginRight: '12px', color: 'white'}}>
+    <Link to={location} style={{ marginRight: "12px", color: "white" }}>
       {props.children}
     </Link>
   );
-}
+};
 
 const Button = (props) => {
   const [isHover, setHover] = useState(false);
 
   return (
-    <button {...props} style={{
-      border: 'none',
-      padding: '19px 32px', backgroundColor: isHover ? '#bfb1c4' : EDMUND_GRAY, cursor: 'pointer', boxShadow: 'none', textShadow: 'none', outline: 'none',
-      width: '150px'
-    }} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>{props.children}</button>
+    <button
+      {...props}
+      style={{
+        border: "none",
+        padding: "19px 32px",
+        backgroundColor: isHover ? "#bfb1c4" : EDMUND_GRAY,
+        cursor: "pointer",
+        boxShadow: "none",
+        textShadow: "none",
+        outline: "none",
+        width: "160px",
+        fontSize: "18px",
+      }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {props.children}
+    </button>
   );
 };
 
 const MidSection = (props) => {
-  const {dir, picture, header, text, button, color} = props;
+  const { dir, picture, header, text, button, color } = props;
+  const pictureOrderClass = dir === "left" ? "order-1" : "order-2";
+  const textOrderClass = dir === "left" ? "order-2" : "order-1";
   return (
-    <section style={{justifyContent: 'space-between', backgroundColor: color, padding: '63px 150px', display: 'flex', flexDirection: dir === 'left' ? 'row' : 'row-reverse'}}>
-      {picture}
-      <div style={{width: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-        <div style={{width: '350px', marginBottom: '16px', fontSize: '45px', fontWeight: 500, lineHeight: '55px', color: 'black'}}>{header}</div>
-        <div style={{marginBottom: '16px', fontSize: '16px', fontWeight: 300, color: 'black'}}>{text}</div>
-        <Button>{button.text}</Button>
+    <section
+      style={{
+        justifyContent: "space-between",
+        backgroundColor: color,
+        padding: `63px ${PAGE_PADDING}`,
+        display: "flex",
+        flexDirection: dir === "left" ? "row" : "row-reverse",
+      }}
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className={`col-md-6 ${pictureOrderClass}`}>{picture}</div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+          className={`col-md-6 ${textOrderClass}`}
+        >
+          <div
+            style={{
+              marginBottom: "16px",
+              fontSize: "45px",
+              fontWeight: 500,
+              lineHeight: "55px",
+              color: "black",
+            }}
+          >
+            {header}
+          </div>
+          <div
+            style={{
+              marginBottom: "16px",
+              fontSize: "16px",
+              fontWeight: 300,
+              color: "black",
+            }}
+          >
+            {text}
+          </div>
+          <Button>{button.text}</Button>
+        </div>
       </div>
     </section>
   );
-}
+};
 
 function App() {
   return (
     <Router>
       <Switch>
-      <Route exact path="/">
-        <Redirect to="/earn" />
-      </Route>
+        <Route exact path="/">
+          <Redirect to="/earn" />
+        </Route>
         <Route exact path="/earn">
           <Earn />
         </Route>
@@ -92,54 +161,109 @@ function App() {
 
 const Earn = () => {
   return (
-    <div style={{height: '100%'}}>
-      <div style={{
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundImage: `url(${HeaderImage})`,
-        height: '450px',
-        padding: '0px 150px',
-        display: 'flex',
-        flexDirection:  'column'
-      }}>
-        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100px'}}>
-          <div style={{color: 'white', fontSize: '25px', fontWeight: 500, lineHeight: '36px'}}><img style={{marginTop: '50px', width: '200px', height: 'auto'}} src={Logo} /></div>
+    <div style={{ height: "100%", width: "100%" }}>
+      <div
+        style={{
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundImage: `url(${HeaderImage})`,
+          height: "450px",
+          padding: `0px ${PAGE_PADDING}`,
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+        }}
+        className="container-fluid"
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: "25px",
+            fontWeight: 500,
+            lineHeight: "36px",
+            marginBottom: "50px",
+          }}
+          className="row"
+        >
+          <div className="col">
+            <img
+              style={{ marginTop: "50px", height: "auto", width: "200px" }}
+              src={Logo}
+            />
+          </div>
         </div>
-        <div style={{width: '450px', display: 'flex', justifyContent: 'center', flexDirection: 'column', flexGrow: 1}}>
-          <div style={{color: 'white', fontSize: '45px', fontWeight: 500, marginBottom: '32px'}}>Make your retail space work for you.</div>
-          <div style={{color: 'white', textDecoration: 'underline'}}>Learn More</div>
+        <div
+          style={{
+            color: "white",
+            fontSize: "45px",
+            fontWeight: 500,
+            marginBottom: "32px",
+          }}
+          clasName="row"
+        >
+          <div className="col-7">Make your retail space work for you.</div>
+        </div>
+        <div
+          style={{ color: "white", textDecoration: "underline" }}
+          className="row"
+        >
+          <div className="col">Learn More</div>
         </div>
       </div>
       <section>
         <MidSection
-          header={'Advertise with us to increase your revenue'}
-          text={'Increase your business revenue by placing digital advertisements in your retail space, with no disruption.'}
-          button={{text: 'Join Us'}}
-          dir={'left'}
-          picture={<img style={{width: '45%', height: 'auto'}} src={BusinessOwnerImage} />}
-          color={'white'}
+          header={"Advertise with us to increase your revenue"}
+          text={
+            "Increase your business revenue by placing digital advertisements in your retail space, with no disruption."
+          }
+          button={{ text: "Join Us" }}
+          dir={"left"}
+          picture={
+            <img
+              style={{ width: "80%", height: "auto" }}
+              src={BusinessOwnerImage}
+            />
+          }
+          color={"white"}
         />
         <MidSection
-          header={'No work involved, just a bit of your space.'}
-          text={'We will source advertisements and parts necessary to display ads in your space. Earn up to 80% of the ad revenue.'}
-          button={{text: 'Learn More'}}
-          dir={'right'}
-          picture={<img style={{width: '45%', height: 'auto'}} src={RelaxImage} />}
+          header={"No work involved, just a bit of your space."}
+          text={
+            "We will source advertisements and parts necessary to display ads in your space. Earn up to 80% of the ad revenue."
+          }
+          button={{ text: "Learn More" }}
+          dir={"right"}
+          picture={
+            <img style={{ width: "80%", height: "auto" }} src={RelaxImage} />
+          }
           color={MIDDLE_BLUE_GREEN}
         />
       </section>
-      <section style={{padding: '63px 150px', backgroundColor: 'white'}}>
-        <div style={{width: '400px'}}>
-          <div style={{fontSize: '45px', color: 'black', marginBottom: '16px'}}>Plans</div>
-          <div style={{fontSize: '16px', color: 'black', marginBottom: '16px'}}>Find the plan that works best for you business.</div>
-        </div>
-        
-        <div style={{display: 'flex', marginBottom: '16px', justifyContent: 'space-between', width: '500px'}}>
-          
+      <section style={{ padding: "63px 150px", backgroundColor: "white" }}>
+        <div style={{ width: "400px" }}>
+          <div
+            style={{ fontSize: "45px", color: "black", marginBottom: "16px" }}
+          >
+            Plans
+          </div>
+          <div
+            style={{ fontSize: "16px", color: "black", marginBottom: "16px" }}
+          >
+            Find the plan that works best for you business.
+          </div>
         </div>
 
-        <table style={{borderCollapse: 'collapse', marginBottom: '32px'}}>
+        <div
+          style={{
+            display: "flex",
+            marginBottom: "16px",
+            justifyContent: "space-between",
+            width: "500px",
+          }}
+        ></div>
+
+        <table style={{ borderCollapse: "collapse", marginBottom: "32px" }}>
           <tr>
             <TH></TH>
             <TH>Type</TH>
@@ -148,24 +272,56 @@ const Earn = () => {
             <TH>One-time Installation</TH>
           </tr>
           <tr>
-            <TD><img src={MountedImage} style={{height: '200px', width: 'auto'}} /></TD>
-            <TD><div>Mounted digital display. We have different dimensions to fit in a variety of spaces.</div></TD>
-            <TD $style={{textAlign: 'center', width: '200px'}}>80% of ad revenue</TD>
-            <TD $style={{textAlign: 'center', width: '200px'}}>$5/mo.</TD>
-            <TD $style={{textAlign: 'center', width: '200px'}}>$260-$460*</TD>
+            <TD>
+              <img
+                src={MountedImage}
+                style={{ height: "200px", width: "auto" }}
+              />
+            </TD>
+            <TD>
+              <div>
+                Mounted digital display. We have different dimensions to fit in
+                a variety of spaces.
+              </div>
+            </TD>
+            <TD $style={{ textAlign: "center", width: "200px" }}>
+              80% of ad revenue
+            </TD>
+            <TD $style={{ textAlign: "center", width: "200px" }}>$5/mo.</TD>
+            <TD $style={{ textAlign: "center", width: "200px" }}>$260-$460*</TD>
           </tr>
           <tr>
-            <TD><img src={StandingImage} style={{height: '200px', width: 'auto'}} /></TD>
-            <TD><div>Free-standing digital display. This gorgeous free standing display will enhance the appearance of your retail space.</div></TD>
-            <TD colspan="3" $style={{textAlign: 'center'}}>Coming soon</TD>
+            <TD>
+              <img
+                src={StandingImage}
+                style={{ height: "200px", width: "auto" }}
+              />
+            </TD>
+            <TD>
+              <div>
+                Free-standing digital display. This gorgeous free standing
+                display will enhance the appearance of your retail space.
+              </div>
+            </TD>
+            <TD colspan="3" $style={{ textAlign: "center" }}>
+              Coming soon
+            </TD>
           </tr>
         </table>
-        <div style={{color: 'gray', fontSize: '12px'}}>* We will work with you to find the display size that fits the best with you space.</div>
+        <div style={{ color: "gray", fontSize: "12px" }}>
+          * We will work with you to find the display size that fits the best
+          with you space.
+        </div>
       </section>
-      <section style={{padding: '63px 38px'}}>
-        contact section
-      </section>
-      <footer style={{padding: '40px', display: 'flex', justifyContent: 'space-between', backgroundColor: '#e1e3ea'}}>
+      <section style={{ padding: "63px 38px" }}>contact section</section>
+      <footer
+        style={{
+          padding: "40px",
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#e1e3ea",
+        }}
+      >
         <div>Slogan</div>
         <div>
           <div>About Us</div>
@@ -176,53 +332,108 @@ const Earn = () => {
       </footer>
     </div>
   );
-}
+};
 
 const Advertise = () => {
   return (
-    <div style={{height: '100%'}}>
-      <div style={{
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundImage: 'url("https://media.discordapp.net/attachments/718995649550352689/719031866082197544/city-meal-food-produce-usa-market-544027-pxhere.com_2.jpg?width=1404&height=936")',
-        height: '450px',
-        backgroundColor: 'blue',
-        padding: '0px 24px',
-        display: 'flex',
-        flexDirection:  'column'
-      }}>
-        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100px'}}>
-          <div style={{color: 'white', fontSize: '25px', fontWeight: 500, lineHeight: '36px'}}>Placeholder</div>
+    <div style={{ height: "100%" }}>
+      <div
+        style={{
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundImage:
+            'url("https://media.discordapp.net/attachments/718995649550352689/719031866082197544/city-meal-food-produce-usa-market-544027-pxhere.com_2.jpg?width=1404&height=936")',
+          height: "450px",
+          backgroundColor: "blue",
+          padding: "0px 24px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            height: "100px",
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              fontSize: "25px",
+              fontWeight: 500,
+              lineHeight: "36px",
+            }}
+          >
+            Placeholder
+          </div>
         </div>
-        <div style={{width: '450px', display: 'flex', justifyContent: 'center', flexDirection: 'column', flexGrow: 1}}>
-          <div style={{color: 'white', fontSize: '45px', fontWeight: 500, marginBottom: '32px'}}>Make your retail space work for you.</div>
-          <div style={{color: 'white', textDecoration: 'underline'}}>Learn More</div>
+        <div
+          style={{
+            width: "450px",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            flexGrow: 1,
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              fontSize: "45px",
+              fontWeight: 500,
+              marginBottom: "32px",
+            }}
+          >
+            Make your retail space work for you.
+          </div>
+          <div style={{ color: "white", textDecoration: "underline" }}>
+            Learn More
+          </div>
         </div>
       </div>
       <section>
         <MidSection
-          header={'Advertise with us to increase your revenue'}
-          text={'Increase your business revenue by placing digital advertisements in your retail space, with no disruption.'}
-          button={{text: 'Join Us'}}
-          dir={'left'}
-          picture={<img style={{width: '45%', height: 'auto'}} src="https://media.discordapp.net/attachments/718995649550352689/719219692417581056/wallpaperflare.com_wallpaper_1.jpg?width=936&height=936" />}
+          header={"Advertise with us to increase your revenue"}
+          text={
+            "Increase your business revenue by placing digital advertisements in your retail space, with no disruption."
+          }
+          button={{ text: "Join Us" }}
+          dir={"left"}
+          picture={
+            <img
+              style={{ width: "45%", height: "auto" }}
+              src="https://media.discordapp.net/attachments/718995649550352689/719219692417581056/wallpaperflare.com_wallpaper_1.jpg?width=936&height=936"
+            />
+          }
         />
         <MidSection
-          header={'No work involved, just a bit of your space.'}
-          text={'We will source advertisements and parts necessary to display ads in your space. Earn up to 80% of the ad revenue.'}
-          button={{text: 'Learn More'}}
-          dir={'right'}
-          picture={<img style={{width: '45%', height: 'auto'}} src="https://cdn.discordapp.com/attachments/718995649550352689/719220796039626752/Untitled_3.jpg" />}
+          header={"No work involved, just a bit of your space."}
+          text={
+            "We will source advertisements and parts necessary to display ads in your space. Earn up to 80% of the ad revenue."
+          }
+          button={{ text: "Learn More" }}
+          dir={"right"}
+          picture={
+            <img
+              style={{ width: "45%", height: "auto" }}
+              src="https://cdn.discordapp.com/attachments/718995649550352689/719220796039626752/Untitled_3.jpg"
+            />
+          }
         />
       </section>
-      <section style={{padding: '63px 38px'}}>
-        pricing
-      </section>
-      <section style={{padding: '63px 38px'}}>
-        contact section
-      </section>
-      <footer style={{padding: '40px', display: 'flex', justifyContent: 'space-between', backgroundColor: '#e1e3ea'}}>
+      <section style={{ padding: "63px 38px" }}>pricing</section>
+      <section style={{ padding: "63px 38px" }}>contact section</section>
+      <footer
+        style={{
+          padding: "40px",
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#e1e3ea",
+        }}
+      >
         <div>Slogan</div>
         <div>
           <div>About Us</div>
@@ -233,6 +444,6 @@ const Advertise = () => {
       </footer>
     </div>
   );
-}
+};
 
 export default App;
